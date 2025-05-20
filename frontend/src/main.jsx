0c7e5fbeda026b31a2e1b709ecc97380e22da72a
ADHARSH M V <120073725+ADHARSH45@users.noreply.github.com>
2025-05-20 14:45:27 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import LandingPage from './pages/login_page.jsx'
+import UI from './ui.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LandingPage />
+    < UI/>
   </StrictMode>,
 )
